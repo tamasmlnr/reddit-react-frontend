c658192e6d6f3897284e02a81f55d4d59b56fc07
Tamás <molnart90@gmail.com>
2019-08-07 17:44:48 +0200

    Add navbar with React Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import postService from './services/PostService'
 import Table from 'react-bootstrap/Table';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import SubmitBlog from './components/SubmitPost'
 import {
   BrowserRouter as Router,
   Route, Link, Redirect, withRouter
 } from 'react-router-dom'
 
+
+const Menu = ({ posts }) => {
+  const padding = {
+    paddingRight: 30
+  }
+
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">all posts</Nav.Link>
+            <Nav.Link href="/post">new post</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Router>
+        <div>
+          <Route exact path="/" render={() => <Posts posts={posts}></Posts>} />
+          <Route path="/post" render={() => <SubmitBlog></SubmitBlog>} />
+          {/* <Route path="/posts/:id" render={({ match }) =>
+          <Anecdote anecdote={anecdoteById(match.params.id)} />
+        } /> */}
+        </div>
+      </Router>
+    </>
+  )
+
+}
 
 const Posts = ({ posts }) => {
   return (
@@ -45,8 +79,7 @@ function App() {
 
   return (
     <>
-      <div> <Posts posts={posts}></Posts> </div>
-      <SubmitBlog></SubmitBlog>
+      <Menu posts={posts}></Menu>
     </>
   );
 }
