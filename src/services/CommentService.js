6219a3e9ce444e592326afbc8ee112e16d0d6386
Tamás <molnart90@gmail.com>
2019-08-10 16:49:23 +0200

    Add commentService

---
+++ src/services/CommentService.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+const url = 'http://localhost:3003/api/comment'
+
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+
+const getAll = async () => {
+  const request = axios.get(url)
+  const response = await request;
+  return response.data;
+}
+
+const getComment = async id => {
+  const request = axios.get(`${url}/${id}`)
+  const response = await request;
+  return response.data;
+}
+
+const updateComment = async (id, newObject) => {
+  const request = axios.put(`${url}/${id}`, newObject)
+  const response = await request
+  return response.data
+}
+
+const create = async newComment => {
+
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.post(url, newComment, config)
+  console.log(newComment);
+  const response = await request;
+  return response.data;
+}
+
+export default {
+  setToken,
+  getAll,
+  getComment,
+  updateComment,
+  create
+}
